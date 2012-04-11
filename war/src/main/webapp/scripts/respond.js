447d21b1ff22ccb3e806b0b71189a6432139446f
Kerry Wilson <kerry.wilson@vanderbilt.edu>
2012-04-10 22:22:22 -0500

    response page

--- war/src/main/webapp/scripts/respond.js
+++ war/src/main/webapp/scripts/respond.js
@@ -1,17 +1,18 @@
 $(function() {
     var gathering, inviteKey=$.w.param('i');
-    $.w.rest.gatheringByInvite(inviteKey,function(g) {
-        gathering=g;
-        var currentVote, tmpl = $('#gatheringTemplate').tmpl(gathering);
-        tmpl.find('#goingBox').click(function() {
-            var placeKey=$(this).data('placeKey');
-            $.w.rest.vote(inviteKey, placeKey, function(){});
-        });
-        currentVote=$(gathering.invitations).filter(function(j,k) { return k.key === inviteKey; })[0].vote;
-        $('.voteCheck').each(function(a,chk) {
-            chk.checked=($(chk).data('placeKey') === currentVote);
-        });
-        
-    });
-   
+    if(!inviteKey) {
+	window.location='/';
+    } else {
+	$.w.rest.gatheringByInvite(inviteKey,function(g) {
+	    gathering=g;
+	    $('#gatheringTemplate').tmpl(gathering)
+	    .appendTo('#respond')
+	    .find('#goingBox').click(function() {
+		$.w.rest.vote(inviteKey, g.places[0].key, function(){});
+	    })[0]
+	    .checked = !!($(gathering.invitations).filter(function(j,k) {
+		return k.key === inviteKey;
+	    })[0].vote);
+	});
+    }
 });
